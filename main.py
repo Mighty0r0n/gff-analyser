40dd4fae4592396f8beabc4a564159b671555469
daniel <daniel.tischler1995@googlemail.com>
2022-06-14 13:36:48 +0200

    hey

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello')
+import argparse
+
+class Gff_column:
+    def __int__(self, row_entrys):
+        self.row_entrys = []
+
+
+        # self.row1 = None
+        # self.row2 = None
+        # self.row3 = None
+        # self.row4 = None
+        # self.row5 = None
+        # self.row6 = None
+        # self.row7 = None
+        # self.row8 = None
+        # self.row9 = None
+        #
