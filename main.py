f7b8f7f38d9296db2a43ab97724393985d559d9e
daniel <daniel.tischler1995@googlemail.com>
2022-06-18 16:02:00 +0200

    hey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse as arg
-import classes.gffClasses as bact
+import classes.gffClasses as Gffclasses
 import time
 
 
@@ -10,16 +10,18 @@ args = parser.parse_args()
 
 
 def bacteria_exists(name: str, bacteria_list: list):
-    return next((True for bacteria in bacteria_list if bacteria.region == name), False)
+    return next((True for bacteria in bacteria_list if bacteria.strain == name), False)
 
 
 def find_bacteria(name: str, bacteria_list: list):
-    return next((bacteria for bacteria in bacteria_list if bacteria.region == name), None)
+    return next((bacteria for bacteria in bacteria_list if bacteria.strain == name), None)
 
 
 def add_sequence(bacteria_list: list, dna_seq: str, fasta_counter: int):
+
     if dna_seq != '':
-        tmp_bacteria: bact.Bacteria = find_bacteria(name=bacteria_list[fasta_counter].region, bacteria_list=bacteria_list)
+        tmp_bacteria: Gffclasses.Organism = find_bacteria(name=bacteria_list[fasta_counter].strain,
+                                                          bacteria_list=bacteria_list)
         tmp_bacteria.fasta += dna_seq
         dna_seq = ''
     return dna_seq
@@ -28,25 +30,26 @@ def add_sequence(bacteria_list: list, dna_seq: str, fasta_counter: int):
 def read():
 
     bacteria_list = []
-    dna_seq_list = []
+    dna_seq = ''
+
     with open(args.file) as gff3:
         fasta_extract = False
 
         fasta_counter = -2
         for line in gff3.readlines():
             if line.startswith("##sequence-region"):
-                lineSplit = line.split(" ")
-                tmp_bacteria = bact.Bacteria()
-                tmp_bacteria.region = lineSplit[1]
+                strain = line.split(" ")
+                tmp_bacteria = Gffclasses.Organism()
+                tmp_bacteria.strain = strain[1]
                 bacteria_list.append(tmp_bacteria)
 
             elif bacteria_exists(line.split("\t")[0], bacteria_list):
-                seq_id = line.split("\t")
-                tmp_bacteria: bact.Bacteria = find_bacteria(name=seq_id[0], bacteria_list=bacteria_list)
-                tmp_bacteria.gff_data.append(bact.GffData(seq_id=seq_id))
+                gffrow = line.split("\t")
+                tmp_bacteria: Gffclasses.Organism = find_bacteria(name=gffrow[0], bacteria_list=bacteria_list)
+                tmp_bacteria.gff_data.append(Gffclasses.GffData(gffrow=gffrow))
 
             elif line.startswith('##FASTA'):
-                dna_seq = ''
+
                 fasta_extract = True
 
             elif line.startswith('>'):
@@ -56,12 +59,10 @@ def read():
             elif fasta_extract and not line.startswith('>'):
                 dna_seq += line.strip('\n')
 
+    add_sequence(bacteria_list=bacteria_list, dna_seq=dna_seq, fasta_counter=fasta_counter + 1)
 
-
-        print('test')
 if __name__ == "__main__":
     start_time = time.time()
 
     read()
     print("--- %s seconds ---" % (time.time() - start_time))
-
